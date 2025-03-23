1eadf98f57556f1f16c56b1b3c3b5b065392b724
Anas Multani <suplexcity930@gmail.com>
2025-03-23 10:06:42 +0530

    standard api classes

--- src/index.js
+++ src/index.js
@@ -6,22 +6,16 @@ import { connectDB } from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
 
-// import express from "express";
-// const app = express();
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     app.on("error", () => {
-//       console.log("Error:", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error: ", error);
-//     throw err;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+  }) 
+  .catch(
+    (errr,
+    () => {
+      console.log("MongoDB connection is failed", err);
+    })
+  );
